727420ffa87df61e16498df8350cd861f058546f
JJMUNPER <jmunper007@g.educaand.es>
2022-04-20 10:08:48 +0200

    2004

--- ListaMusica/src/main/java/es/juanjesusmunozperez/listamusica/App.java
+++ ListaMusica/src/main/java/es/juanjesusmunozperez/listamusica/App.java
@@ -6,8 +6,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -20,13 +29,28 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-       HBox root = new HBox();
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: #d5ffff;"); //Cambiar el color
+       /*HBox root = new HBox();
        root.setAlignment(Pos.CENTER);
-       root.setSpacing(10);
+       root.setSpacing(10);*/
        
        var scene = new Scene(root, 640, 480);
        stage.setScene(scene);
        stage.show();
+       
+        VBox VBox = new VBox();
+        VBox.setAlignment(Pos.CENTER);
+        VBox.setBorder(new Border(new BorderStroke(Color.valueOf("#5dc1b9"),
+        BorderStrokeStyle.SOLID,
+        CornerRadii.EMPTY,
+        new BorderWidths(15))));
+        VBox.setSpacing(20);
+        root.getChildren().add(VBox);
+        
+       
+        
+        
        
         Cancion cancion1 = new Cancion("Rock", "Clasic");
         //System.out.println(cancion1.getTitulo());
@@ -37,12 +61,12 @@ public class App extends Application {
         cancion1.setPrecio(1.50F);
         cancion1.setFechaPublicacion("08/10/1971");
         
-        System.out.println(cancion1.getTitulo());
+        /*System.out.println(cancion1.getTitulo());
         System.out.println(cancion1.getAutor());
         System.out.println(cancion1.getDiscografica());
         System.out.println(cancion1.getAlbum());
         System.out.println(cancion1.getPrecio());
-        System.out.println(cancion1.getFechaPublicacion());
+        System.out.println(cancion1.getFechaPublicacion());*/
         
         
         Cancion cancion2 = new Cancion("Rock", "Classic");
@@ -53,12 +77,12 @@ public class App extends Application {
         cancion2.setPrecio(1.78F);
         cancion2.setFechaPublicacion("1973");
         
-        System.out.println(cancion2.getTitulo());
+        /*System.out.println(cancion2.getTitulo());
         System.out.println(cancion2.getAutor());
         System.out.println(cancion2.getDiscografica());
         System.out.println(cancion2.getAlbum());
         System.out.println(cancion2.getPrecio());
-        System.out.println(cancion2.getFechaPublicacion());
+        System.out.println(cancion2.getFechaPublicacion());*/
         
         
         Cancion cancion3 = new Cancion("Country", "Classic");
@@ -71,12 +95,12 @@ public class App extends Application {
         //cancion3.setPrecio(25.10F);
         cancion3.setFechaPublicacion("1969");
         
-        System.out.println(cancion3.getTitulo());
+        /*System.out.println(cancion3.getTitulo());
         System.out.println(cancion3.getAutor());
         System.out.println(cancion3.getDiscografica());
         System.out.println(cancion3.getAlbum());
         System.out.println(cancion3.getPrecio());
-        System.out.println(cancion3.getFechaPublicacion());
+        System.out.println(cancion3.getFechaPublicacion());*/
         
         Canciones canciones = new Canciones();
         canciones.getListaCancion().add(cancion1);
@@ -85,6 +109,7 @@ public class App extends Application {
         
         
         Button buttonSelecFile = new Button ("Guardar XML");
+        //buttonSelecFile.setGraphic(new ImageView(new Image("src/main/resources/save.png")));
         root.getChildren().add(buttonSelecFile);
         buttonSelecFile.setOnAction((t) ->{       
             UtilXML.guardarDatosXML(stage, canciones);
@@ -105,7 +130,20 @@ public class App extends Application {
 
         });
         
-        // Esto esta hecho a modo como ejemplo; Corregirlo para hacerlo de otra forma
+         //Diseño botones/Pos
+        
+        HBox paneScores = new HBox();
+        paneScores.setAlignment(Pos.TOP_CENTER);
+        paneScores.getChildren().add(buttonSelecFile);
+        paneScores.getChildren().add(buttonSelectAbrir);
+        paneScores.setSpacing(200);
+         paneScores.setBorder(new Border(new BorderStroke(Color.valueOf("#5dc1b9"),
+        BorderStrokeStyle.SOLID,
+        CornerRadii.EMPTY,
+        new BorderWidths(3))));
+        VBox.getChildren().add(paneScores);
+        
+        // Labels para mostrar por pantalla.
         //
         //
         //
@@ -114,14 +152,23 @@ public class App extends Application {
         Label label2 = new Label();
         //Label label3 = new Label();
         
-        root.getChildren().add(label);
+        /*root.getChildren().add(label);
         root.getChildren().add(label1);
         root.getChildren().add(label2);
-        //root.getChildren().add(label3);
+        root.getChildren().add(label3);*/
+        
+        //Mostrar los Labels por pantalla
+        
+        root.getChildren().addAll(label, label1, label2);
         
         
         Button buttonAnterior = new Button ("Anterior");
         root.getChildren().add(buttonAnterior);
+        buttonAnterior.setStyle("-fx-background-color: #5dc1b9;");
+            buttonAnterior.setMinHeight(40);
+            buttonAnterior.setMinWidth(100);
+            buttonAnterior.setMaxHeight(40);
+            buttonAnterior.setMaxWidth(100);
         buttonAnterior.setOnAction((t) -> {
             cancionActual --;
             try {
@@ -129,8 +176,8 @@ public class App extends Application {
                 label1.setText("Autor: " + canciones.getListaCancion().get(cancionActual).getAutor());
                 label2.setText("Discografica: " + canciones.getListaCancion().get(cancionActual).getDiscografica());
             }catch (Exception ex) {
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.setTitle("Error; no existen mas datos");
+                Alert alert = new Alert(Alert.AlertType.WARNING); //Cambiar el tipo de Alert por Information
+                alert.setTitle("Error; no existen mas datos");    //public static final Alert.AlertType.INFORMATION
                 alert.setContentText("Ya estas en la primera cancion");
                 alert.showAndWait();
             }
@@ -159,10 +206,7 @@ public class App extends Application {
             label1.setText("Autor: " + canciones.getListaCancion().get(cancionActual).getAutor());
             label2.setText("Discografica: " + canciones.getListaCancion().get(cancionActual).getDiscografica());
         
-        //Lectura de csv
-        //Guardar en csv
-        //Basicamente hacer el procesamiento de ficheros de texto, es el apartado de la moodle
-        //Añadir tambien boton de guardar
+        
     }
 
     public static void main(String[] args) {
