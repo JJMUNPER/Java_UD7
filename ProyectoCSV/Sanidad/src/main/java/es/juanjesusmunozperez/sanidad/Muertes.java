303ad0328c0ada14a50c4160188fbe1c899d0895
JJMUNPER <jmunper007@g.educaand.es>
2022-04-27 10:03:00 +0200

    2704.2

--- ProyectoCSV/Sanidad/src/main/java/es/juanjesusmunozperez/sanidad/Muertes.java
+++ ProyectoCSV/Sanidad/src/main/java/es/juanjesusmunozperez/sanidad/Muertes.java
@@ -13,8 +13,49 @@ public class Muertes {
     
     private String Pais;
     private String Iniciales;
-    private String Año;
+    private int Anno;
     private String CausasMuerte;
+    
+    public Muertes (String Pais){
+        this.Pais = Pais.toUpperCase();
+    }
+    
+    public Muertes (String Pais, String Causa){  //Causa en miniscula; autor
+        this.Pais = Pais.toUpperCase();
+        this.Iniciales = Iniciales;
+    }
+    
+    public String getPais(){
+        return Pais;
+    }
+    
+    public void setPais(String Pais){
+        this.Pais = Pais.toUpperCase();
+    }
+    
+    public String getIniciales(){
+        return Iniciales;
+    }
+    
+    public void setIniciales (String Iniciales){
+        this.Iniciales = Iniciales;
+    }
+    
+    public int getAnno (){
+        return Anno;
+    }
+    
+    public void setAnno(int Anno){
+        this.Anno = Anno;
+    }
+    
+    public String CausasMuerte(){
+        return CausasMuerte;
+    }
+    
+    public void CausasMuerte(String CausasMuerte){
+        this.CausasMuerte = CausasMuerte;
+    }
     
     
     
